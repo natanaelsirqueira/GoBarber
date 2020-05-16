547b233c3e66cff20eb9f603c2bad6e0d5266858
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-05-16 12:02:18 -0400

    Minor changes

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,5 +1,5 @@
 declare namespace Express {
   export interface Request {
-    user: { id: string }
+    user: { id: string };
   }
 }
--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -3,4 +3,4 @@ export default {
     secret: 'fe4a9bde72c50bf976d64550af5b44e6',
     expiresIn: '1d',
   },
-}
+};
