dba4e8c429209af5abb77e1060d94af39b054843
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-05-18 18:59:43 -0400

    Added rate limiting

---
+++ backend/src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import Redis from 'ioredis';
+
+import cacheConfig from '@config/cache';
+import AppError from '@shared/errors/AppError';
+
+const redisClient = new Redis(cacheConfig.config.redis);
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5, // 5 requests
+  duration: 1, // per 1 second by IP
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests', 429);
+  }
+}
