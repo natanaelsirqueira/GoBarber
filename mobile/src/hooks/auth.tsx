5aba852123c13b02e75e8252669d63be518c0c7e
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-06-11 14:58:22 -0400

    Mobile Dashboard

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -9,6 +9,13 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
@@ -16,11 +23,11 @@ interface SignInCredentials {
 
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -40,6 +47,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token && user) {
+        api.defaults.headers.authorization = `Bearer ${token}`;
+
         setData({ token, user: JSON.parse(user) });
       }
 
@@ -58,6 +67,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
